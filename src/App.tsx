12ce0bc6b9f125a71f898a3ad45f995906bf163c
Vadim Kiniabaev <thekinv21@gmail.com>
2024-07-04 16:18:39 +0300

    Shadn UI kütühanesi eklendi ve button component gösterildi

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from './components/ui/button'
 export default function App() {
 	return (
 		<section className='w-full h-screen flex items-center justify-center'>
-			<Button variant='outline' className='px-10 py-2'>
+			<Button variant='default' className='px-10 py-2'>
 				Click me
 			</Button>
 		</section>
