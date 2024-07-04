9659a2b20dcbdbefbdc36f719f03f915d75dc031
Vadim Kiniabaev <thekinv21@gmail.com>
2024-07-04 16:24:03 +0300

    Prettier eklendi

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Button } from './components/ui/button'
 
 export default function App() {
 	return (
-		<section className='w-full h-screen flex items-center justify-center'>
+		<section className='flex h-screen w-full items-center justify-center'>
 			<Button variant='default' className='px-10 py-2'>
 				Click me
 			</Button>
